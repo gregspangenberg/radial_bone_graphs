e4795ea207244b44596808f7a5d844c184eaf132
greg <gspangen@westerneng.ca>
2022-03-16 12:20:09 -0400

    subplots fixed legend

--- radial_graph.py
+++ radial_graph.py
@@ -14,24 +14,30 @@ load_dir = 45
 implant_dir = 'STD'
 # cortical remodelling
 df_graph = df[(df['posi'] == implant_dir) & (df['bone_type'] == 'Cortical') & (df['load'] == load_dir)]
-fig1 = px.bar_polar(df_graph, r="remodel", theta="quad",
-                   color="depth", template="plotly_dark",
-                   color_discrete_sequence= px.colors.sequential.Plasma_r)
+fig1 = px.bar_polar(df_graph, r="remodel", theta="quad", color="depth",
+                    title = 'Cortical Remodelling',
+                    template="plotly_dark",
+                    color_discrete_sequence= px.colors.sequential.Plasma_r)
 # trabecular remodelling
 df_graph = df[(df['posi'] == implant_dir) & (df['bone_type'] == 'Trabecular') & (df['load'] == load_dir)]
-fig2 = px.bar_polar(df_graph, r="remodel", theta="quad",
-                   color="depth", template="plotly_dark",
-                   color_discrete_sequence= px.colors.sequential.Plasma_r)
+fig2 = px.bar_polar(df_graph, r="remodel", theta="quad", color="depth",
+                    title = 'Trabecular Remodelling',
+                    template="plotly_dark",
+                    color_discrete_sequence= px.colors.sequential.Plasma_r)
 # cortical resorption
 df_graph = df[(df['posi'] == implant_dir) & (df['bone_type'] == 'Cortical') & (df['load'] == load_dir)]
-fig3 = px.bar_polar(df_graph, r="resorp", theta="quad",
-                   color="depth", template="plotly_dark",
-                   color_discrete_sequence= px.colors.sequential.Plasma_r)
+fig3 = px.bar_polar(df_graph, r="resorp", theta="quad", color="depth",
+                    title = 'Cortical Resorption',
+                    template="plotly_dark",
+                    color_discrete_sequence= px.colors.sequential.Plasma_r)
+
 # trabecular resorption
 df_graph = df[(df['posi'] == implant_dir) & (df['bone_type'] == 'Trabecular') & (df['load'] == load_dir)]
-fig4 = px.bar_polar(df_graph, r="resorp", theta="quad",
-                   color="depth", template="plotly_dark",
-                   color_discrete_sequence= px.colors.sequential.Plasma_r)
+fig4 = px.bar_polar(df_graph, r="resorp", theta="quad", color="depth",
+                    title = 'Trabecular Resorption',
+                    template="plotly_dark",
+                    color_discrete_sequence= px.colors.sequential.Plasma_r)
+
 
 # For as many traces that exist per Express figure, get the traces from each plot and store them in an array.
 # This is essentially breaking down the Express fig into it's traces
@@ -42,10 +48,13 @@ fig4_traces = []
 for trace in range(len(fig1["data"])):
     fig1_traces.append(fig1["data"][trace])
 for trace in range(len(fig2["data"])):
+    fig2["data"][trace]['showlegend'] = False # removes extra legends
     fig2_traces.append(fig2["data"][trace])
 for trace in range(len(fig3["data"])):
+    fig3["data"][trace]['showlegend'] = False # removes extra legends
     fig3_traces.append(fig3["data"][trace])
 for trace in range(len(fig4["data"])):
+    fig4["data"][trace]['showlegend'] = False # removes extra legends
     fig4_traces.append(fig4["data"][trace])
 
 big_fig = sp.make_subplots(rows=2, cols=2, specs=[[{'type': 'polar'}]*2]*2) 
@@ -60,4 +69,5 @@ for traces in fig3_traces:
 for traces in fig4_traces:
     big_fig.append_trace(traces, row=2, col=2)
 
+# print(fig1_traces[0]['showlegend'])
 big_fig.show()
