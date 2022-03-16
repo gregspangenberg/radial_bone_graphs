880cb71133d1dc9063beb2f66de8073fe1c836c1
greg <gspangen@westerneng.ca>
2022-03-15 20:07:47 -0400

    basic graph

--- radial_graph.py
+++ radial_graph.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
+import plotly.express as px
 import pathlib 
 
 def chunks(l,n):
@@ -27,8 +28,6 @@ df.insert(0,'depth',(['0-5','5-10','10-15','15-20','20-25','25-30','30-35','35-4
 
 # reduce all specimens into just average
 df = df.groupby('depth',sort=False).mean()
-
-# print(df)
 
 # concatenate into 3-tuple of all specimens
 i = 0
@@ -84,3 +83,10 @@ df['posi'] = np.select(conditions, choices)
 
 # df.to_csv('data.csv')
 # print(df)
+
+# build graphs
+df_graph = df[(df['posi'] == 'STD') & (df['bone_type'] == 'Cortical') & (df['load'] == '45')]
+fig = px.bar_polar(df_graph, r="resorb", theta="quad",
+                   color="depth", template="plotly_dark",
+                   color_discrete_sequence= px.colors.sequential.Plasma_r)
+fig.show()
